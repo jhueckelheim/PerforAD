b4f7b351c34d8a4d5ae99fc9f9c719cbd740f7bb
Jan H <j.c.hueckelheim@qmul.ac.uk>
2019-04-15 21:15:03 +0100

    C output pretty-printer

--- stencildiff.py
+++ stencildiff.py
@@ -2,7 +2,8 @@ import sympy as sp
 import textwrap
 from operator import itemgetter
 
-#verboseprint = print if verbose else lambda *a, **k: None
+verbose = False
+verboseprint = print if verbose else lambda *a, **k: None
 
 class LoopNest:
   def __init__(self,body,bounds):
@@ -30,14 +31,14 @@ class LoopNest:
     # This loop goes over all dimensions. In each dimension, nestlist is replaced
     # with a new nestlist that has been split in that dimension.
     for counter in self.counters:
-      print("counter %s"%counter)
+      verboseprint("counter %s"%counter)
       newnestlist = []
       # This loop goes over all nests. Each nest may get split into several new
       # nests, which are appended to newnestlist.
       for nest,nestbound in nestlist:
-        print("  nest %s"%str(nestbound))
+        verboseprint("  nest %s"%str(nestbound))
         nest.sort(key=lambda x: x[0][counter])
-        print("  -with offs %s"%list(map(lambda x: x[0],nest)))
+        verboseprint("  -with offs %s"%list(map(lambda x: x[0],nest)))
         # Multiple offsets may hav the same offset in the current dimension. We need to find
         # the positions in the array where the offset in this dimension changes.
         offsets = list(map(lambda x: x[0][counter],nest))
@@ -57,8 +58,8 @@ class LoopNest:
           bounds_post = nestbound.copy()
           bounds_pre[counter] = nestbound[counter][0]+offs_1-1,nestbound[counter][0]+offs_0
           bounds_post[counter] = nestbound[counter][1]+offs_1,nestbound[counter][1]+offs_0+1
-          print("    pre %s"%bounds_pre)
-          print("    post %s"%bounds_post)
+          verboseprint("    pre %s"%bounds_pre)
+          verboseprint("    post %s"%bounds_post)
           # Append the nest to the new list of nests
           newnestlist.append((stmts_pre,bounds_pre))
           newnestlist.append((stmts_post,bounds_post))
@@ -66,7 +67,7 @@ class LoopNest:
         stmts_core = nest
         bounds_core = nestbound.copy()
         bounds_core[counter] = nestbound[counter][0]+nest[-1][0][counter],nestbound[counter][1]+nest[0][0][counter]
-        print("    core %s"%bounds_core)
+        verboseprint("    core %s"%bounds_core)
         newnestlist.append((stmts_core,bounds_core))
       # Replace the old nest list with the refined one, ready for the next iteration
       nestlist = newnestlist
@@ -74,7 +75,7 @@ class LoopNest:
     loops = []
     for body_b,nestbound in nestlist:
       statements = map(itemgetter(1),body_b)
-      print(nestbound)
+      verboseprint(nestbound)
       loops.append(LoopNest(statements,nestbound))
     return loops
 
@@ -109,12 +110,12 @@ class _Loop_:
       # Try and simplify loops with only one iteration (print the loop
       # body, and assign the correct value to the counter variable).
       if(self.end-self.start==0):
-        return "%s=%s\n%s"%(self.counter,self.start,res)
+        return "%s=%s;\n%s"%(self.counter,self.start,res)
     except TypeError:
       # If that failed, perhaps the loop bounds are again symbolic. Print the
       # loop, everything will be fine at runtime.
       pass
-    return "do %s=%s,%s\n%s\nend do"%(self.counter,self.start,self.end,textwrap.indent(str(res),4*" "))
+    return "for ( %s=%s; %s<=%s; %s++ ) {\n%s\n}"%(self.counter,self.start,self.counter,self.end,self.counter,textwrap.indent(str(res),4*" "))
 
 class SympyFuncStencil:
   def __init__(self, name, args):
@@ -126,7 +127,7 @@ class SympyFuncStencil:
 
   def at(self,inputs):
     argstr = ", ".join(map(lambda arg: "%s=%s"%(arg[0],arg[1]), zip(self.args,inputs)))
-    return "%s(%s)"%(self.name,argstr)
+    return "%s[%s]"%(self.name,argstr)
 
   def diff(self,wrt):
     diffname = "%s_d"%self.name
@@ -176,8 +177,8 @@ class StencilExpression:
           idxlist.append(dim+of)
         args.append(var(*idxlist))
       lhsargs = ",".join(map(lambda x: str(x),self.idx_out))
-      lhs = "%s[%s]"%(self.outvar,lhsargs)
-    return "%s += %s"%(lhs,self.func.at(args))
+      lhs = "%s(%s)"%(self.outvar,lhsargs)
+    return "%s += %s;"%(lhs,self.func.at(args))
 
   def diff(self,diffvars):
     # All invars and offsets given to the StencilExpression are
@@ -223,19 +224,25 @@ outv_b = sp.Function('outv_b')
 inv_b = sp.Function('inv_b')
 vel_b = sp.Function('vel_b')
 
-f = SympyFuncStencil("f",[l,c,r])
-stexpr = StencilExpression(outv, [inv], [i], [[[-1],[0],[1]]],f)
-loop1d = LoopNest(body=stexpr, bounds={i:[2,n-1]})
-print(loop1d)
-for lp in (loop1d.diff({inv:inv_b, outv:outv_b})):
-  print(lp)
+def printcpp(varnames):
+  for varname in varnames:
+    print("#define %s(...) %s[__VA_ARGS__]"%(varname,varname))
 
-f = SympyExprStencil(l+r-2*c*a,[l,c,r,a])
-stexpr = StencilExpression(outv, [inv,vel], [i], [[[-1],[0],[1]],[[0]]],f)
-loop1d = LoopNest(body=stexpr, bounds={i:[2,n-1]})
-print(loop1d)
-for lp in (loop1d.diff({inv:inv_b, outv:outv_b})):
-  print(lp)
+printcpp([outv,inv,vel,inv_b,outv_b])
+
+#f = SympyFuncStencil("foo",[l,c,r])
+#stexpr = StencilExpression(outv, [inv], [i], [[[-1],[0],[1]]],f)
+#loop1d = LoopNest(body=stexpr, bounds={i:[2,n-1]})
+#print(loop1d)
+#for lp in (loop1d.diff({inv:inv_b, outv:outv_b})):
+#  print(lp)
+#
+#f = SympyExprStencil(l+r-2*c*a,[l,c,r,a])
+#stexpr = StencilExpression(outv, [inv,vel], [i], [[[-1],[0],[1]],[[0]]],f)
+#loop1d = LoopNest(body=stexpr, bounds={i:[2,n-1]})
+#print(loop1d)
+#for lp in (loop1d.diff({inv:inv_b, outv:outv_b})):
+#  print(lp)
 
 f = SympyExprStencil((l+r-2*c)*a,[l,c,r,a])
 stexpr = StencilExpression(outv, [inv,vel], [i], [[[-1],[0],[1]],[[0]]],f)
@@ -243,22 +250,23 @@ loop1d = LoopNest(body=stexpr, bounds={i:[2,n-1]})
 print(loop1d)
 for lp in (loop1d.diff({inv:inv_b, outv:outv_b, vel:vel_b})):
   print(lp)
+#for lp in (loop1d.diff({inv:inv_b, outv:outv_b})):
 
-f2d = SympyFuncStencil("f",[l,b,c,r,t,a])
-stexpr2d = StencilExpression(outv, [inv,vel], [i,j], [[[-1,0],[0,-1],[0,0],[1,0],[0,1]],[[0,0]]],f2d)
-loop2d = LoopNest(body=stexpr2d, bounds={i:[2,n-1],j:[2,n-1]})
-print(loop2d)
-for lp in (loop2d.diff({inv:inv_b, outv:outv_b})):
-  print(lp)
-
-f = SympyExprStencil(sp.Piecewise((c-l, l > r), (r-c, True)),[l,c,r])
-stexpr = StencilExpression(outv, [inv], [i], [[[-1],[0],[1]]],f)
-loop1d = LoopNest(body=stexpr, bounds={i:[2,n-1]})
-print(loop1d)
-for lp in (loop1d.diff({inv:inv_b, outv:outv_b})):
-  print(lp)
-
-f2d = SympyExprStencil(l+b+r+t-4*c*a,[l,b,c,r,t,a])
+#f2d = SympyFuncStencil("f",[l,b,c,r,t,a])
+#stexpr2d = StencilExpression(outv, [inv,vel], [i,j], [[[-1,0],[0,-1],[0,0],[1,0],[0,1]],[[0,0]]],f2d)
+#loop2d = LoopNest(body=stexpr2d, bounds={i:[2,n-1],j:[2,n-1]})
+#print(loop2d)
+#for lp in (loop2d.diff({inv:inv_b, outv:outv_b})):
+#  print(lp)
+#
+#f = SympyExprStencil(sp.Piecewise((c-l, l > r), (r-c, True)),[l,c,r])
+#stexpr = StencilExpression(outv, [inv], [i], [[[-1],[0],[1]]],f)
+#loop1d = LoopNest(body=stexpr, bounds={i:[2,n-1]})
+#print(loop1d)
+#for lp in (loop1d.diff({inv:inv_b, outv:outv_b})):
+#  print(lp)
+#
+f2d = SympyExprStencil((l+b+r+t-4*c)*a,[l,b,c,r,t,a])
 stexpr2d = StencilExpression(outv, [inv,vel], [i,j], [[[-1,0],[0,-1],[0,0],[1,0],[0,1]],[[0,0]]],f2d)
 loop2d = LoopNest(body=stexpr2d, bounds={i:[2,n-1],j:[2,n-1]})
 print(loop2d)
